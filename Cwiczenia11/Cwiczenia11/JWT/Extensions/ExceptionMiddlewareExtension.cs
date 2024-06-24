41abe0f86f1cc1959dea6b3fda6ef23f7c378c44
s28557 <s28557@pjwstk.edu.pl>
2024-06-24 19:26:20 +0200

    commit 01

---
+++ Cwiczenia11/Cwiczenia11/JWT/Extensions/ExceptionMiddlewareExtension.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace JWT.Extensions;
+
+public static class ExceptionMiddlewareExtension
+{
+    public static void ConfigureExceptionHandler(this IApplicationBuilder appBuilder)
+    {
+        appBuilder.UseExceptionHandler(appError =>
+        {
+            appError.Run(async context =>
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                context.Response.ContentType = "application/json";
+
+                var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                if (contextFeature != null)
+                {
+                    var logger = appBuilder.ApplicationServices.GetService(typeof(ILogger<Program>)) as ILogger;
+                    logger.LogError($"Something went wrong: {contextFeature.Error}");
+
+                    await context.Response.WriteAsync(new
+                    {
+                        StatusCode = context.Response.StatusCode,
+                        Message = "Internal Server Error. Please try again later."
+                    }.ToString());
+                }
+            });
+        });
+    }
+}
