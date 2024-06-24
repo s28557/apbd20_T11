5906100db711e811c7430e5d32372ce47d114c47
s28557 <s28557@pjwstk.edu.pl>
2024-06-24 20:32:34 +0200

    commit 02

--- Cwiczenia11/Cwiczenia11/JWT/Middlewares/CustomExceptionHandler.cs
+++ Cwiczenia11/Cwiczenia11/JWT/Middlewares/CustomExceptionHandler.cs
@@ -1,4 +1,6 @@
-﻿namespace JWT.Middlewares;
+﻿using System.Net.Http.Headers;
+using System.Text;
+namespace JWT.Middlewares;
 
 public class CustomExceptionHandler
 {
@@ -8,15 +10,41 @@ public class CustomExceptionHandler
         _next = next;
     }
     
-    // Implement exception handling here
     public async Task InvokeAsync(HttpContext context)
     {
-        // Code to execute before the next middleware
-        Console.WriteLine($"Request: {context.Request.Method} {context.Request.Path}");
-        
-        await _next(context);
-        
-        // Code to execute after the next middleware
-        Console.WriteLine($"Response: {context.Response.StatusCode}");
+        try
+        {
+            if (context.Request.Headers.ContainsKey("Authorization"))
+            {
+                var authorizationHeader = AuthenticationHeaderValue.Parse(context.Request.Headers["Authorization"]!);
+
+                if (authorizationHeader.Scheme.Equals("basic", StringComparison.OrdinalIgnoreCase) && authorizationHeader.Parameter != null)
+                {
+                    var credentials = Encoding.UTF8.GetString(Convert.FromBase64String(authorizationHeader.Parameter)).Split(':');
+                    var userName = credentials[0];
+                    var userPassword = credentials[1];
+
+                    if (IsUserAuthorized(userName, userPassword))
+                    {
+                        await _next(context);
+                        return;
+                    }
+                }
+            }
+
+            context.Response.Headers["WWW-Authenticate"] = $"Basic realm=\"Authentication\"";
+            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+        }
+        catch (Exception ex)
+        {
+            // Log the exception (implement logging as needed)
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            await context.Response.WriteAsync("An unexpected error occurred. Please try again later.");
+        }
+    }
+
+    private bool IsUserAuthorized(string userName, string userPassword)
+    {
+        return userName == "admin" && userPassword == "qwerty";
     }
 }
